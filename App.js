25676dacfb3fc43d2b9954688f7b974a1994104b
JEONGSUJI <wtw9611@gmail.com>
2020-04-13 20:51:26 +0900

    bugfix: react tab navigation

--- App.js
+++ App.js
@@ -1,17 +1,66 @@
 import React from 'react';
-import { View } from 'react-native';
-import AddEntry from './components/AddEntry';
+import { View, Platform, StatusBar } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import AddEntry from './components/AddEntry';
 import History from './components/History';
+
+import { white, purple } from './utils/colors';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
+const Tabs =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{ flex: 1 }}>
         <View style={{ height: 20 }} />
-        <History />
+        <NavigationContainer>
+          <Tabs.Navigator
+            initialRouteName="AddEntry"
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ color }) => {
+                let icon;
+                if (route.name === 'Add Entry') {
+                  icon = (
+                    <FontAwesome name="plus-square" size={30} color={color} />
+                  );
+                } else if (route.name === 'Add Entry') {
+                  icon = (
+                    <FontAwesome name="ios-bookmarks" size={30} color={color} />
+                  );
+                }
+                return icon;
+              },
+            })}
+            tabBarOptions={{
+              activeTintColor: Platform.OS === 'ios' ? purple : white,
+              style: {
+                height: 80,
+                backgroundColor: Platform.OS === 'ios' ? white : purple,
+                shadowColor: 'rgba(0, 0, 0, 0.24)',
+                shadowOffset: {
+                  width: 0,
+                  height: 3,
+                },
+                shadowRadius: 6,
+                shadowOpacity: 1,
+              },
+            }}
+          >
+            <Tabs.Screen name="History" component={History} />
+            <Tabs.Screen name="AddEntry" component={AddEntry} />
+          </Tabs.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   );
