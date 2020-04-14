95297798039de732eb600fb06097fc57a4fabb6a
JEONGSUJI <wtw9611@gmail.com>
2020-04-15 06:22:31 +0900

    edit Entry Detail View

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,13 +1,52 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+import MetricCard from './MetricCard';
+import { white } from '../utils/colors';
+import TextButton from './TextButton';
+import { addEntry } from '../actions';
+import { removeEntry } from '../utils/api';
 
-const EntryDetail = (props) => {
-  return (
-    <View>
-      <Text>Entry Detail</Text>
-      {/* {this.props.navigation.state.params.entryId} */}
-    </View>
-  );
-};
+class EntryDetail extends Component {
+  reset = () => {
+    const { remove, goBack, entryId } = this.props;
 
-export default EntryDetail;
+    remove();
+    goBack();
+    removeEntry(entryId);
+  };
+  shouldComponentUpdate(nextProps) {
+    return nextProps.metrics !== null && !nextProps.metrics.today;
+  }
+  render() {
+    const { metrics } = this.props;
+    return (
+      <View style={styles.container}>
+        <MetricCard metrics={metrics} />
+
+        <TextButton style={{ margin: 20 }} onPress={this.reset}>
+          RESET
+        </TextButton>
+      </View>
+    );
+  }
+}
+
+function mapStateToProps(state, { route }) {
+  const { entryId } = route.params;
+
+  return {
+    entryId,
+    metrics: state[entryId],
+  };
+}
+
+export default connect(mapStateToProps)(EntryDetail);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: white,
+    padding: 15,
+  },
+});
--- components/HistoryStack.js
+++ components/HistoryStack.js
@@ -4,13 +4,31 @@ import { createStackNavigator } from '@react-navigation/stack';
 import History from './History';
 import EntryDetail from './EntryDetail';
 
+import { white } from '../utils/colors';
+
 const Stack = createStackNavigator();
 
 const HistoryStack = () => {
   return (
-    <Stack.Navigator initialRouteName="History" headerMode="none">
+    <Stack.Navigator
+      initialRouteName="History"
+      headerMode="none"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: white,
+        },
+      }}
+    >
       <Stack.Screen name="History" component={History} />
-      <Stack.Screen name="EntryDetail" component={EntryDetail} />
+      <Stack.Screen
+        name="EntryDetail"
+        component={EntryDetail}
+        options={({ route }) => {
+          return {
+            title: route.params.entryId,
+          };
+        }}
+      />
     </Stack.Navigator>
   );
 };
